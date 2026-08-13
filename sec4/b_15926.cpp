9b9d10d019699b9dd1131d645ad79e54abbbd818
HanSupace <101040547+HanSupace@users.noreply.github.com>
2026-08-13 23:20:39 +0900

    8/13

---
+++ sec4/b_15926.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, ret=-987654321;
+string s;
+stack<int> stk;
+
+int main(){
+    cin >> n;
+    cin >> s;
+    stk.push(-1);
+    for(int i=0 ; i<n ; i++){
+        if(s[i]=='('){
+            stk.push(i);
+        }
+        else if(s[i]==')'){
+            stk.pop();
+            if(!stk.empty()){
+                ret = max(ret, i-stk.top());
+            }
+            else{
+                stk.push(i);
+            }
+        }
+    }
+    if(ret == -987654321) cout << 0;
+    else cout << ret;
+}
