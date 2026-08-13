a15cf9c6ab5abfb676d2e59d2aa5f45bbf2e794d
HanSupace <101040547+HanSupace@users.noreply.github.com>
2026-08-13 21:51:31 +0900

    8/13

---
+++ sec4/b_14405.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    int flag=0;
+    cin >> s;
+    for(int i=0 ; i<s.size() ; i++){
+        if(i<s.size()-1 && s.substr(i,2)=="pi" || s.substr(i,2)=="ka"){
+            i++;
+        }
+        else if(i<s.size()-2 && s.substr(i,3)=="chu"){
+            i+=2;
+        }
+        else flag=1;
+    }
+
+    if(flag) cout << "NO\n";
+    else cout << "YES\n";
+}
---
+++ sec4/b_5430.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t,n,x;
+string p,s;
+deque<int> d;
+
+int main(){
+    cin >> t;
+    while(t--){
+        cin >> p >> n >> s;
+        x=0;
+        for(char c : s){
+            if(c=='[' || c==']') continue;
+            else if(c>='0' && c<='9'){
+                x = x*10+c-'0'; 
+            }
+            else{
+                d.push_back(x);
+                x=0;
+            }
+        }
+        if(x > 0) d.push_back(x);
+
+        bool rev = false;
+        bool error = false;
+
+        for(char c : p){
+            if(c=='R') rev = !rev;
+            if(c=='D'){
+                if(d.empty()){
+                    error=true;
+                    break;
+                }
+                else{
+                    if(rev){
+                        d.pop_back();
+                    }
+                    else{
+                        d.pop_front();
+                    }
+                }
+            }
+        }
+        if(error) cout << "error\n";
+        else{
+            cout << '[';
+            if(rev) reverse(d.begin(), d.end());
+
+            for(int i=0 ; i<d.size() ; i++){
+                cout << d[i];
+                if(i!=d.size()-1) cout <<",";
+            }
+            cout << "]\n";
+        }
+            
+
+        
+
+        d.clear();
+
+    }
+}
